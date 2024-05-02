cbd6540d0657faafb090ce48712920e5f5949ca8
forward0606 <73343386+forward0606@users.noreply.github.com>
2024-05-02 22:37:27 +0800

    delete self loop and multi edge

--- HW5/p4/p4.cpp
+++ HW5/p4/p4.cpp
@@ -39,7 +39,7 @@ vector<pair<int, int>> v;
 void build(int n){
     v.clear();
     for(int i=1;i<=n;i++){
-        for(int j=1;j<=n;j++){
+        for(int j=i+1;j<=n;j++){
             v.emplace_back(i, j);
         }
     }
